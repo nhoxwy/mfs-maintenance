c24aee9aef25cce9a343b367154b38a0a334fc26
Chi Linh <chilinhnguyen101@gmail.com>
2021-10-13 19:04:36 +0700

    MFS Maintenance Start

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a href="www.facebook.com/thptndc.thanhxuan">
+      <img src="~/static/maintenance.png">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  width: auto;
+  height: 100vh;
+}
+</style>
